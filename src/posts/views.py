6c4104e015af8326c00bd2a6b0236bc323b7ad94
rameshkonatala <konatalaramesh@gmail.com>
2017-03-17 20:40:19 +0530

    Lesson 24 | Templates and inheritance

--- src/posts/views.py
+++ src/posts/views.py
@@ -34,7 +34,7 @@ def post_list(request):
 		"object_list":queryset,
 		"title":"List"
 	}
-	return render(request,"index.html",context)
+	return render(request,"post_list.html",context)
 	#return HttpResponse("<h1>List</h1>")
 
 def post_update(request,id=None):
