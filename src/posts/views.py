e4f3ce44aa3674d3d5e4f7d8992e04cb85005e6a
rameshkonatala <konatalaramesh@gmail.com>
2017-03-17 14:54:34 +0530

    Lesson:18 | Dynamic URL

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,17 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
-
+from .models import Post
 # Create your views here.
 
 
 def post_create(request):
 	return HttpResponse("<h1>Create</h1>")
 
-def post_detail(request):
-	return HttpResponse("<h1>Detail</h1>")
+def post_detail(request,id):
+	instance  = get_object_or_404(Post,id=id)
+	context={
+		"title":instance.title,
+		"instance":instance,
+	}
+	return render(request,"post_detail.html",context)
 
 def post_list(request):
-	return render(request,"index.html",{})
+	queryset = Post.objects.all()
+	context={
+		"object_list":queryset,
+		"title":"List"
+	}
+	return render(request,"index.html",context)
 	#return HttpResponse("<h1>List</h1>")
 
 def post_update(request):
