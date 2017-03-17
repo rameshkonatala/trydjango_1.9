c6aca4ba670ff01f6d711cd71d768a1033d45c78
rameshkonatala <konatalaramesh@gmail.com>
2017-03-17 20:15:55 +0530

    Lesson 23 | Delete view

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render,get_object_or_404
-from django.http import HttpResponse
+from django.shortcuts import render,get_object_or_404,redirect
+from django.http import HttpResponse,HttpResponseRedirect
 from .models import Post
 from .forms import PostForm
+from django.contrib import messages
 # Create your views here.
 
 
@@ -10,6 +11,10 @@ def post_create(request):
 	if form.is_valid():
 		instance = form.save(commit=False)
 		instance.save()
+		messages.success(request,"Successfully created")
+		return HttpResponseRedirect(instance.get_absolute_url())
+	else:
+		messages.error(request,"Not Successfully created")
 	context = {
 		"form":form,
 	}
@@ -32,8 +37,23 @@ def post_list(request):
 	return render(request,"index.html",context)
 	#return HttpResponse("<h1>List</h1>")
 
-def post_update(request):
-	return HttpResponse("<h1>Update</h1>")
+def post_update(request,id=None):
+	instance = get_object_or_404(Post,id=id)
+	form = PostForm(request.POST or None,instance=instance)
+	if form.is_valid():
+		instance = form.save(commit=False)
+		instance.save()
+		messages.success(request,"Item updated")
+		return HttpResponseRedirect(instance.get_absolute_url())		
+	context={
+		"title":instance.title,
+		"instance":instance,
+		"form":form,
+	}
+	return render(request,"post_form.html",context)
 
-def post_delete(request):
-	return HttpResponse("<h1>Delete</h1>")
+def post_delete(request,id=None):
+	instance = get_object_or_404(Post,id=id)
+	instance.delete()
+	messages.success(request,"Item deleted")
+	return redirect("posts:lists")
