68e959ee4a5f3fbb6f6b6a1271846c4934a272fb
rameshkonatala <konatalaramesh@gmail.com>
2017-03-18 11:40:10 +0530

    Lesson: 27 | Pagination for blogs

--- src/posts/views.py
+++ src/posts/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse,HttpResponseRedirect
 from .models import Post
 from .forms import PostForm
 from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import render
 # Create your views here.
 
 
@@ -29,13 +31,32 @@ def post_detail(request,id):
 	return render(request,"post_detail.html",context)
 
 def post_list(request):
-	queryset = Post.objects.all()
+	queryset_list = Post.objects.all()#.order_by("-timestamp")
+	paginator = Paginator(queryset_list, 2) # Show 25 contacts per page
+	page_request_var = 'set'
+	page = request.GET.get(page_request_var)
+	try:
+	    queryset = paginator.page(page)
+	except PageNotAnInteger:
+	    # If page is not an integer, deliver first page.
+	    queryset = paginator.page(1)
+	except EmptyPage:
+	    # If page is out of range (e.g. 9999), deliver last page of results.
+	    queryset = paginator.page(paginator.num_pages)
+
 	context={
 		"object_list":queryset,
-		"title":"List"
+		"title":"List",
+		"page_request_var":page_request_var
 	}
 	return render(request,"post_list.html",context)
 	#return HttpResponse("<h1>List</h1>")
+
+
+
+def listing(request):
+    contact_list = Contacts.objects.all()
+
 
 def post_update(request,id=None):
 	instance = get_object_or_404(Post,id=id)
