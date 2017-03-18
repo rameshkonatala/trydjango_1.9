d44da29650bd8a8d41fa30e4ff4be56cf2b7a944
rameshkonatala <konatalaramesh@gmail.com>
2017-03-18 14:35:10 +0530

    Lesson: 29 | Slug in URL

--- src/posts/views.py
+++ src/posts/views.py
@@ -22,8 +22,8 @@ def post_create(request):
 	}
 	return render(request,"post_form.html",context)
 
-def post_detail(request,id):
-	instance  = get_object_or_404(Post,id=id)
+def post_detail(request,slug):
+	instance  = get_object_or_404(Post,slug=slug)
 	context={
 		"title":instance.title,
 		"instance":instance,
@@ -58,8 +58,8 @@ def listing(request):
     contact_list = Contacts.objects.all()
 
 
-def post_update(request,id=None):
-	instance = get_object_or_404(Post,id=id)
+def post_update(request,slug=None):
+	instance = get_object_or_404(Post,slug=slug)
 	form = PostForm(request.POST or None,request.FILES or None,instance=instance)
 	if form.is_valid():
 		instance = form.save(commit=False)
@@ -73,8 +73,8 @@ def post_update(request,id=None):
 	}
 	return render(request,"post_form.html",context)
 
-def post_delete(request,id=None):
-	instance = get_object_or_404(Post,id=id)
+def post_delete(request,slug=None):
+	instance = get_object_or_404(Post,slug=slug)
 	instance.delete()
 	messages.success(request,"Item deleted")
 	return redirect("posts:lists")
