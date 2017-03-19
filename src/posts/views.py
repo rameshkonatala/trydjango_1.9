fc60943f5f708fba1f8d1b2441b7b1f1a17efd40
rameshkonatala <konatalaramesh@gmail.com>
2017-03-19 10:47:00 +0530

    Lesson: 33 | User authentication and Associate user to post

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,6 +1,6 @@
 from urllib import quote_plus
 from django.shortcuts import render,get_object_or_404,redirect
-from django.http import HttpResponse,HttpResponseRedirect
+from django.http import HttpResponse,HttpResponseRedirect,Http404
 from .models import Post
 from .forms import PostForm
 from django.contrib import messages
@@ -10,9 +10,13 @@ from django.shortcuts import render
 
 
 def post_create(request):
+	if not request.user.is_staff or not request.user.is_superuser:
+		raise Http404
+
 	form = PostForm(request.POST or None, request.FILES or None)
 	if form.is_valid():
 		instance = form.save(commit=False)
+		instance.user = request.user
 		instance.save()
 		messages.success(request,"Successfully created")
 		return HttpResponseRedirect(instance.get_absolute_url())
@@ -62,6 +66,8 @@ def listing(request):
 
 
 def post_update(request,slug=None):
+	if not request.user.is_staff or not request.user.is_superuser:
+		raise Http404
 	instance = get_object_or_404(Post,slug=slug)
 	form = PostForm(request.POST or None,request.FILES or None,instance=instance)
 	if form.is_valid():
@@ -77,6 +83,8 @@ def post_update(request,slug=None):
 	return render(request,"post_form.html",context)
 
 def post_delete(request,slug=None):
+	if not request.user.is_staff or not request.user.is_superuser:
+		raise Http404	
 	instance = get_object_or_404(Post,slug=slug)
 	instance.delete()
 	messages.success(request,"Item deleted")
